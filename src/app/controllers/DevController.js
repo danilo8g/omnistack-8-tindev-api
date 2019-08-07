f368fe37c6d3f5796020b85ad921f90a14802b1a
Danilo <gonca.danilo@gmail.com>
2019-08-06 23:32:08 -0300

    Updated DevController

--- src/app/controllers/DevController.js
+++ src/app/controllers/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios')
 const Dev = require('../models/Dev')
 
 class DevController {
+  async index (req, res) {
+    const { user } = req.headers
+
+    const loggedDev = await Dev.findById(user)
+
+    const devs = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    })
+
+    return res.status(200).json(devs)
+  }
+
   async store (req, res) {
     const { username } = req.body
     const response = await axios.get(`https://api.github.com/users/${username}`)
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const routes = require('express').Router()
 
 const controllers = require('./app/controllers')
 
-routes.post('/dev', controllers.DevController.store)
-routes.post('/dev/:id/like', controllers.LikeController.store)
-routes.post('/dev/:id/dislike', controllers.DislikeController.store)
+routes.get('/devs', controllers.DevController.index)
+routes.post('/devs', controllers.DevController.store)
+routes.post('/devs/:id/like', controllers.LikeController.store)
+routes.post('/devs/:id/dislike', controllers.DislikeController.store)
 
 module.exports = routes
