b839740c99143575ea61afa427da29d8013fc420
Danilo <gonca.danilo@gmail.com>
2019-08-06 21:43:00 -0300

    Create DevController

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ const routes = require('express').Router()
 
 const controllers = require('./app/controllers')
 
-routes.get('/dev', controllers.DevController.store)
+routes.post('/dev', controllers.DevController.store)
 
 module.exports = routes
