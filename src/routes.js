f658f390763ad29bc71c4a4fdd1ec7d2b49c7000
Danilo <gonca.danilo@gmail.com>
2019-08-06 23:04:29 -0300

    Created DislikeController

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const controllers = require('./app/controllers')
 
 routes.post('/dev', controllers.DevController.store)
 routes.post('/dev/:id/like', controllers.LikeController.store)
+routes.post('/dev/:id/dislike', controllers.DislikeController.store)
 
 module.exports = routes
